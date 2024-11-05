a2eeb72240265b5e6bee61983b0ce2f2b420d179
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2024-11-05 10:37:59 -0300

    Create 1006.py

---
+++ 1006/1006.py
@@ -0,0 +1,8 @@
+num_func = int(input())
+hora_trab = int(input())
+valor_hora = float(input())
+
+salario = hora_trab * valor_hora
+
+print(f"NUMBER = {num_func}")
+print("SALARY = U$ {:.2f}".format(salario))
