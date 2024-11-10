a244977706af73b56533df0614af9a2e878a6388
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2024-11-10 17:13:12 -0300

    Update 1018.py

--- 1018/1018.py
+++ 1018/1018.py
@@ -1,1 +1,19 @@
-as
+valor = int(input())
+
+cem = valor // 100
+cinquenta = valor % 100 // 50
+vinte = valor % 100 % 50 // 20
+dez = valor % 100 % 50 % 20 // 10
+cinco = valor % 100 % 50 % 20 % 10 // 5
+dois = valor % 100 % 50 % 20 % 10 % 5 // 2
+um = valor % 100 % 50 % 20 % 10 % 5 % 2 // 1
+
+
+print(f"{valor}")
+print(f"{cem} nota(s) de R$ 100,00")
+print(f"{cinquenta} nota(s) de R$ 50,00")
+print(f"{vinte} nota(s) de R$ 20,00")
+print(f"{dez} nota(s) de R$ 10,00")
+print(f"{cinco} nota(s) de R$ 5,00")
+print(f"{dois} nota(s) de R$ 2,00")
+print(f"{um} nota(s) de R$ 1,00")
