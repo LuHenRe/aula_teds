640c6a55f8790124464dc4bd8fbcc11ddc79a82c
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2024-11-16 15:58:05 -0300

    Create 1079.py

---
+++ 1079 - Médias Ponderadas (Torneio)/1079.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+medias = []
+
+for _ in range (n):
+    x, y, z = map(float, input().split())
+    media = (x * 2 + y * 3 + z * 5) / 10
+    medias.append(media)
+
+for media in medias:
+    print(f"{media:.1f}")
