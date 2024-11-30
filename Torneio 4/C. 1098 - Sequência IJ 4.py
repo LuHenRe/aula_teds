13151f3fcb65eac410d206f5671287f11fafa18b
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2024-11-30 13:20:53 -0300

    Create C. 1098 - Sequência IJ 4.py

---
+++ Torneio 4/C. 1098 - Sequência IJ 4.py
@@ -0,0 +1,18 @@
+i = 0
+while i < 1:
+    print(f"I={round(i, 1)} J={round(i + 1, 1)}")    
+    print(f"I={round(i, 1)} J={round(i + 2, 1)}")
+    print(f"I={round(i, 1)} J={round(i + 3, 1)}")
+    i += 0.2
+
+i = 1
+while i < 1.9:
+    print(f"I={round(i, 1)} J={round(i + 1, 1)}")    
+    print(f"I={round(i, 1)} J={round(i + 2, 1)}")
+    print(f"I={round(i, 1)} J={round(i + 3, 1)}")
+    i += 0.2
+
+i = 2
+print(f"I={round(i, 1)} J={round(i + 1, 1)}")    
+print(f"I={round(i, 1)} J={round(i + 2, 1)}")
+print(f"I={round(i, 1)} J={round(i + 3, 1)}")
